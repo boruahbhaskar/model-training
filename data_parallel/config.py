3d77586d82920e256a4fb5ca4c9b3903628eeddf
Bhaskar Boruah <boruah.bhaskar@gmail.com>
2024-03-01 13:33:35 -0600

    added data parallel code committed by Bhaskar

---
+++ data_parallel/config.py
@@ -0,0 +1,28 @@
+import torchvision.models as models
+
+# define hyperpparameters 
+
+do_data_parallel = True
+
+per_device_batch_size = 8
+
+learning_rate = 8
+
+epochs = 1
+
+model_name = models.resnet152()
+
+num_classes = 10
+
+data_dir = '~/imagenette2'
+
+device = 'cuda'
+
+memory_limit = 1.0
+
+visible_devices = [0 , 1 , 2, 3]
+
+MLFLOW_TRACKING_URI = 'http://localhost:5000'
+
+MLFLOW_EXPERIMENT_ID = 2
+
